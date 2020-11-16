c9212dda2a970f47302b036ea919214a17967c47
sunjh <1455364690@qq.com>
2020-11-16 20:11:09 +0800

    add metrix

--- src/main/java/com/sunjh/xiyiji/XiyijiApplication.java
+++ src/main/java/com/sunjh/xiyiji/XiyijiApplication.java
@@ -1,9 +1,7 @@
 package com.sunjh.xiyiji;
 
-import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -13,11 +11,6 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 public class XiyijiApplication {
     public static void main(String[] args) {
         SpringApplication.run(XiyijiApplication.class, args);
-    }
-    @Bean
-    MeterRegistryCustomizer<MeterRegistry> configurer(
-            @Value("${spring.application.name}") String applicationName) {
-        return (registry) -> registry.config().commonTags("application", applicationName);
     }
 
 }
--- src/main/java/com/sunjh/xiyiji/aspect/LogControllerAspect.java
+++ src/main/java/com/sunjh/xiyiji/aspect/LogControllerAspect.java
@@ -3,20 +3,30 @@ package com.sunjh.xiyiji.aspect;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.subject.Subject;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.multipart.MultipartFile;
+import sun.security.provider.MD5;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.sql.Timestamp;
 import java.util.Arrays;
+import java.util.StringJoiner;
 
 /**
  * @author sunjh
@@ -28,73 +38,70 @@ import java.util.Arrays;
 @Slf4j
 public class LogControllerAspect {
     private final static Logger logger = LoggerFactory.getLogger(LogControllerAspect.class);
-
-    /** 以 controller 包下定义的所有请求为切入点 */
+    /**
+     * 以 controller 包下定义的所有请求为切入点
+     */
     @Pointcut("execution(public * com.sunjh.xiyiji.controller..*.*(..))")
-    public void webLog() {}
-
+    public void controllerLog() {
+    }
+    @Pointcut("execution(public * com.sunjh.xiyiji.dao..*.*(..))")
+    public void daoLog() {
+    }
+    @Pointcut("execution(public * com.sunjh.xiyiji.logic..*.*(..))")
+    public void logicLog() {
+    }
+    @Pointcut("execution(public * com.sunjh.xiyiji.service..*.*(..))")
+    public void serviceLog() {
+    }
     /**
      * 在切点之前织入
+     *
      * @param joinPoint
      * @throws Throwable
      */
-    @Before("webLog()")
-    public void doBefore(JoinPoint joinPoint) {
-        // 开始打印请求日志
+    //@Before("controllerLog() || logicLog() || serviceLog() || daoLog()")
+    public void doBefore(ProceedingJoinPoint joinPoint) {
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = attributes.getRequest();
-
+        MethodSignature signature = (MethodSignature)joinPoint.getSignature();
+        Method method = signature.getMethod();
         Object[] args = joinPoint.getArgs();
-
-        Object[] arguments  = new Object[args.length];
-        for (int i = 0; i < args.length; i++) {
-            if (args[i] instanceof ServletRequest || args[i] instanceof ServletResponse || args[i] instanceof MultipartFile) {
-                //ServletRequest不能序列化，从入参里排除，否则报异常：java.lang.IllegalStateException: It is illegal to call this method if the current request is not in asynchronous mode (i.e. isAsyncStarted() returns false)
-                //ServletResponse不能序列化 从入参里排除，否则报异常：java.lang.IllegalStateException: getOutputStream() has already been called for this response
-                continue;
-            }
-            arguments[i] = args[i];
-        }
-        String paramter = "";
-        if (arguments != null) {
-            try {
-                paramter = JSONObject.toJSONString(arguments);
-            } catch (Exception e) {
-                paramter = Arrays.toString(arguments);
+        String[] names = signature.getParameterNames();
+        Class[] classes = signature.getParameterTypes();
+        StringJoiner stringJoiner = new StringJoiner(",","[","]");
+        for (int i = 0; i < names.length; i++) {
+            if(isString(args[i]) || isPrimitive(args[i]) || classes[i].getName().contains("cn.iselab")) {
+                stringJoiner.add(names[i] + ":"+ args[i]);
+            } else {
+                stringJoiner.add(names[i]);
             }
         }
-
         // 打印请求相关参数
         logger.info("========================================== Start ==========================================");
         // 打印请求 url
-        logger.info("URL            : {}", request.getRequestURL().toString());
-        // 打印 Http method
-        logger.info("HTTP Method    : {}", request.getMethod());
-        // 打印调用 controller 的全路径以及执行方法
-        logger.info("Class Method   : {}.{}", joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName());
-        // 打印请求的 IP
-        logger.info("IP             : {}", request.getRemoteAddr());
-        // 打印请求入参
-        logger.info("Request Args   : {}", paramter);
+        logger.info("[Request]{URL:{} ; HttpMethod:{} ; ClassMethod: {}.{} ; IP:{}} ; RequestArgs:{}", request.getRequestURL().toString(), request.getMethod(), joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName(),request.getRemoteAddr(),stringJoiner.toString());
     }
 
     /**
      * 在切点之后织入
+     *
      * @throws Throwable
      */
-    @After("webLog()")
-    public void doAfter() throws Throwable {
+    //@After("controllerLog() || logicLog() || serviceLog() || daoLog()")
+    public void doAfter(){
         logger.info("=========================================== End ===========================================");
     }
 
     /**
      * 环绕
+     *
      * @param proceedingJoinPoint
      * @return
      * @throws Throwable
      */
-    @Around("webLog()")
+    @Around("controllerLog() || logicLog() || serviceLog() || daoLog()")
     public Object doAround(ProceedingJoinPoint proceedingJoinPoint) {
+        doBefore(proceedingJoinPoint);
         long startTime = System.currentTimeMillis();
         Object result = null;
         try {
@@ -104,9 +111,20 @@ public class LogControllerAspect {
             logger.debug(throwable.getMessage());
         }
         // 打印出参
-        logger.info("Response Args  : {}", JSON.toJSONString(result));
-        // 执行耗时
-        logger.info("Time-Consuming : {} ms", System.currentTimeMillis() - startTime);
+        logger.info("[Response]{ResponseArgs:{};Time-Consuming : {} ms}", JSON.toJSONString(result),System.currentTimeMillis() - startTime);
+        doAfter();
         return result;
+    }
+
+    private static boolean isPrimitive(Object obj) {
+        try {
+            return ((Class<?>) obj.getClass().getField("TYPE").get(null)).isPrimitive();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private static boolean isString(Object obj) {
+        return obj instanceof String;
     }
 }
