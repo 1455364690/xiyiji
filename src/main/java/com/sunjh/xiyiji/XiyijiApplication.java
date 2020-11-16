b54659ca920b3dab51f30edb039e17fad2c5c01b
sunjh <1455364690@qq.com>
2020-11-16 14:36:34 +0800

    add metrix

--- src/main/java/com/sunjh/xiyiji/XiyijiApplication.java
+++ src/main/java/com/sunjh/xiyiji/XiyijiApplication.java
@@ -1,15 +1,24 @@
 package com.sunjh.xiyiji;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy(proxyTargetClass=true,exposeProxy=true)
 public class XiyijiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(XiyijiApplication.class, args);
     }
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> configurer(
+            @Value("${spring.application.name}") String applicationName) {
+        return (registry) -> registry.config().commonTags("application", applicationName);
+    }
 
 }
+
