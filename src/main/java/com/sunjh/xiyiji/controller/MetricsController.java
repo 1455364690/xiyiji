f48c0a2068594ed65d9301067e366fad4c83b577
sunjh <1455364690@qq.com>
2020-11-16 13:50:51 +0800

    .

---
+++ src/main/java/com/sunjh/xiyiji/controller/MetricsController.java
@@ -0,0 +1,18 @@
+package com.sunjh.xiyiji.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author sunjh
+ * @version 1.0
+ * @date 2020/11/16 1:49 下午
+ */
+@RestController
+public class MetricsController extends BaseController{
+    @GetMapping("metrics")
+    public String metrics(){
+        return "test";
+    }
+
+}
