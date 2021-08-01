b568b9694e0941ccded89db88d9e89666e3345d6
sunjh <1455364690@qq.com>
2021-08-01 20:13:27 +0800

    注释掉定时任务

--- src/main/java/com/sunjh/xiyiji/logic/impl/SkywalkingLogicImpl.java
+++ src/main/java/com/sunjh/xiyiji/logic/impl/SkywalkingLogicImpl.java
@@ -1,8 +1,15 @@
 package com.sunjh.xiyiji.logic.impl;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.sunjh.xiyiji.logic.SkywalkingLogic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author sunjh
@@ -11,8 +18,32 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class SkywalkingLogicImpl implements SkywalkingLogic {
-    @Scheduled(cron = "*/5 * * * * ?")
-    public void test(){
-        System.out.println("ttttttt");
+
+    @Value("${skywalking.datasource}")
+    private String skywalkingUrl;
+
+//    @Scheduled(cron = "*/5 * * * * ?")
+//    public void test() {
+//        System.out.println("ttttttt");
+//    }
+
+    public JSONObject doPostForObject(String requestUrl, JSONObject requestBody) {
+        RestTemplate rt = new RestTemplate();
+        HttpEntity<JSONObject> httpEntity = new HttpEntity<>(requestBody, getHeader());
+        JSONObject response = rt.postForObject(requestUrl, httpEntity, JSONObject.class);
+        return null;
+    }
+
+    public JSONArray doPostForArray(String requestUrl, JSONObject requestBody) {
+        RestTemplate rt = new RestTemplate();
+        HttpEntity<JSONObject> httpEntity = new HttpEntity<>(requestBody, getHeader());
+        JSONArray response = rt.postForObject(requestUrl, httpEntity, JSONArray.class);
+        return null;
+    }
+
+    private HttpHeaders getHeader() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return headers;
     }
 }
